f62c264851b10b3d8a56bada93ea1b70fa506c44
misaelsarm <misaelsarmiento97@gmail.com>
2020-05-14 01:23:53 -0500

    latest

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,4 +1,4 @@
-/* const config = {
+const config = {
   apiKey: 'AIzaSyD27LqhyI_NP4k3A2Nc3aYf8O-Zoy_PpKQ',
   authDomain: 'ciscoe-cdf48.firebaseapp.com',
   databaseURL: 'https://ciscoe-cdf48.firebaseio.com',
@@ -7,8 +7,8 @@
   messagingSenderId: '119518087187',
   appId: '1:119518087187:web:e6a32a051d616b4b7a57e6',
   measurementId: 'G-C3SFBLRHG3'
-}; */
-var config = {
+};
+/* var config = {
     apiKey: "AIzaSyDyZMgqM3cfPFGlImLcbH3FUcgRp1VXuMI",
     authDomain: "ciscoe-291e5.firebaseapp.com",
     databaseURL: "https://ciscoe-291e5.firebaseio.com",
@@ -17,6 +17,6 @@ var config = {
     messagingSenderId: "256113730166",
     appId: "1:256113730166:web:eb5edde1658cf55f1bb557",
     measurementId: "G-B64D7S4W4E"
-  };
+  }; */
 
 export default config;
